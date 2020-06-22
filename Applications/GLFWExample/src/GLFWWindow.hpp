09760925da5eb29c1a1a30eb3ba7bfcb2bcc6189
DaftMat <mathis.margot98@gmail.com>
2020-06-22 03:25:39 +0200

    refactor core libs and add materials to them

--- Applications/GLFWExample/src/GLFWWindow.hpp
+++ Applications/GLFWExample/src/GLFWWindow.hpp
@@ -2,13 +2,13 @@
 // Created by mathis on 24/05/2020.
 //
 #pragma once
-
-
 #include <GLFW/glfw3.h>
+
+#include <API.hpp>
 #include <functional>
 
 class ENGINE_API GLFWWindow {
-public:
+   public:
     GLFWWindow(int width, int height);
     ~GLFWWindow() { terminate(); }
 
@@ -16,14 +16,18 @@ public:
 
     void finish();
 
-    static void setFramebufferCallback(std::function<void(int,int)> framebufferCallback)
-    { m_framebufferCallback = std::move(framebufferCallback); }
-private:
+    static void setFramebufferCallback(std::function<void(int, int)> framebufferCallback) {
+        m_framebufferCallback = std::move(framebufferCallback);
+    }
+
+   private:
     void terminate();
 
     GLFWwindow *m_window;
 
-    static std::function<void(int,int)> m_framebufferCallback;
+    static std::function<void(int, int)> m_framebufferCallback;
 
-    static void framebuffer_size_callback(GLFWwindow *window, int pwidth, int pheight) { m_framebufferCallback(pwidth, pheight); }
+    static void framebuffer_size_callback(GLFWwindow *window, int pwidth, int pheight) {
+        m_framebufferCallback(pwidth, pheight);
+    }
 };
