aebcb4c62bd9b408ec2177ad814b89902c527a29
DaftMat <mathis.margot98@gmail.com>
2020-08-29 16:25:46 +0200

    add documentation to camera

--- src/Engine/Renderer/Cameras/Camera.hpp
+++ src/Engine/Renderer/Cameras/Camera.hpp
@@ -8,12 +8,16 @@
 namespace daft::engine {
 class ENGINE_API Camera {
    public:
+    /**
+     * struct representing a mouse.
+     */
     struct Mouse {
-        glm::vec2 pos;
-        enum class Button { Left, Right, Wheel } button;
+        glm::vec2 pos;                                    ///< current position of the mouse.
+        enum class Button { Left, Right, Wheel } button;  ///< button that was pressed.
     };
 
-    /** Constructor.
+    /**
+     * Constructor.
      * no roll as this camera won't need it.
      * @param position - position of the camera
      * @param worldUp - upward direction of the camera
@@ -23,38 +27,65 @@ class ENGINE_API Camera {
     explicit Camera(const glm::vec3& position = {0.f, 0.f, -3.f}, const glm::vec3& worldUp = {0.f, 1.f, 0.f},
                     float yaw = 90.f, float pitch = 0.f);
 
-    /** calculates view matrix.
-     *
+    /**
+     * calculates view matrix.
      * @return the view matrix of the camera.
      */
     [[nodiscard]] glm::mat4 getViewMatrix() const { return glm::lookAt(m_position, m_position + m_front, m_up); }
 
-    /** fov getter.
-     *
+    /**
+     * fov getter.
      * @return fov (zoom) of the camera.
      */
     [[nodiscard]] float fov() const { return m_fov; }
 
-    /** position getter.
-     *
+    /**
+     * position getter.
      * @return position of the camera.
      */
     [[nodiscard]] const glm::vec3& position() const { return m_position; }
 
-    /** front getter.
-     *
+    /**
+     * front getter.
      * @return the direction the camera faces.
      */
     [[nodiscard]] const glm::vec3& viewDir() const { return m_front; }
 
+    /**
+     * Apply action related to mouse scroll event.
+     * Makes the distance between the camera's position and target vary.
+     * @param offset - change of the scroll position.
+     */
     void processMouseScroll(float offset);
 
+    /**
+     * Apply action related to mouse press event.
+     * Updates mouse data.
+     * @param mouse
+     */
     void processMousePress(Mouse mouse);
 
+    /**
+     * Apply action related to mouse release event.
+     * Updates mouse data.
+     */
     void processMouseRelease() { m_mousePressed = false; }
 
+    /**
+     * Apply action related to mouse move event.
+     * The action depends on the button that was pressed.
+     *  Left button - the camera rotates around the target.
+     *  Right button - the camera translates following its up and right vectors.
+     *  Wheel button - the camera translates following its front vector.
+     * @param mousePos
+     */
     void processMouseMove(glm::vec2 mousePos);
 
+    /**
+     * Flips the camera.
+     * Inverse the yaw and the y position.
+     * Useful for reflection effects on a plane surface.
+     */
     void flip();
 
    private:
