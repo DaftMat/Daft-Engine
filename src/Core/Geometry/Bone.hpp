f8f216835f9569ce767b649166a49c0da81000b4
DaftMat <mathis.margot98@gmail.com>
2020-12-26 14:58:04 +0100

    add documentation

--- src/Core/Geometry/Bone.hpp
+++ src/Core/Geometry/Bone.hpp
@@ -7,27 +7,70 @@
 #include <Core/Utils/Types.hpp>
 
 namespace daft::core {
+/**
+ * Represents an articulation.
+ */
 class Bone : public NonCopyable {
    public:
+    /**
+     * Constructor.
+     * @param startPos - starting position of the bone.
+     * @param endPos - ending position of the bone.
+     */
     explicit Bone(glm::vec3 startPos = glm::vec3{0.f}, glm::vec3 endPos = glm::vec3{0.f})
         : m_startPos{startPos}, m_endPos{endPos} {}
 
+    /**
+     * Destructor.
+     */
     ~Bone() = default;
 
+    /**
+     * Move constructor.
+     */
     Bone(Bone &&) noexcept = default;
 
+    /**
+     * Move assignment operator.
+     * @return ref to this.
+     */
     Bone &operator=(Bone &&) noexcept = default;
 
+    /**
+     * Starting position getter.
+     * @return Starting position of the bone.
+     */
     [[nodiscard]] glm::vec3 startPos() const { return m_startPos; }
 
+    /**
+     * Ending position getter.
+     * @return Ending position of the bone.
+     */
     [[nodiscard]] glm::vec3 endPos() const { return m_endPos; }
 
+    /**
+     * Mean position getter.
+     * @return The position of the center of the bone.
+     */
     [[nodiscard]] glm::vec3 meanPos() const { return (m_startPos + m_endPos) / 2.f; }
 
+    /**
+     * Rotations getter.
+     * The vector represents the rotation angle around each of the 3 axis X, Y, Z.
+     * @return a vector of rotations.
+     */
     [[nodiscard]] glm::vec3 rotations() const { return m_rotations; }
 
+    /**
+     * Rotations reference getter.
+     * @return ref to vector of rotations.
+     */
     glm::vec3 &rotations() { return m_rotations; }
 
+    /**
+     * Calculates the transformation matrix of the bone.
+     * @return model matrix of the bone.
+     */
     [[nodiscard]] glm::mat4 modelMatrix() const;
 
    private:
